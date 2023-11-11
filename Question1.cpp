4f3f47876984992f84eb24e974f13644365ec2bd
PouyaGohari <pouya.hmgohari@gmail.com>
2023-11-11 11:24:40 +0330

    Getting input from user

--- Question1.cpp
+++ Question1.cpp
@@ -3,7 +3,17 @@
 
 using namespace std;
 
+vector<int> getting_input(){
+    int number;
+    cin >> number;
+    vector<int> result(number);
+    for(int i = 0; i < number; i++){
+        cin >> result[i];
+    }
+    return result;
+}
+
 int main(){
-    
+    vector<int> inputs = getting_input();
     return 0;
 }
