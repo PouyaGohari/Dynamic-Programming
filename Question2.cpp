9ae15ad3143a54dc75e1b88486c773b1aa8dac4c
PouyaGohari <pouya.hmgohari@gmail.com>
2023-11-12 11:59:08 +0330

    Refactor the getting input function

--- Question2.cpp
+++ Question2.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 struct fish_street{
+    int number;
     vector<int> fishes;
     vector<int> streets;
 }typedef fish_street;
@@ -11,7 +12,8 @@ struct fish_street{
 fish_street getting_input(){
     int number;
     cin >> number;
-    fish_street my_inputs;  
+    fish_street my_inputs;
+    my_inputs.number = number;  
     for(int i = 0; i < number; i++){
         int temp;
         cin >> temp;
@@ -26,6 +28,6 @@ fish_street getting_input(){
 }
 
 int main(){
-
+    fish_street inputs = getting_input();
     return 0;
 }
