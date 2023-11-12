48af9d366517251c117246c6013aff0fc70eb8ad
PouyaGohari <pouya.hmgohari@gmail.com>
2023-11-12 11:54:46 +0330

    Create a fucntion to getting inputs from user

--- Question2.cpp
+++ Question2.cpp
@@ -3,7 +3,29 @@
 
 using namespace std;
 
+struct fish_street{
+    vector<int> fishes;
+    vector<int> streets;
+}typedef fish_street;
+
+fish_street getting_input(){
+    int number;
+    cin >> number;
+    fish_street my_inputs;  
+    for(int i = 0; i < number; i++){
+        int temp;
+        cin >> temp;
+        my_inputs.fishes.push_back(temp);
+    }
+    for(int i = 0; i < number; i++){
+        int temp;
+        cin >> temp;
+        my_inputs.streets.push_back(temp);
+    }
+    return my_inputs;
+}
+
 int main(){
-    
+
     return 0;
 }
