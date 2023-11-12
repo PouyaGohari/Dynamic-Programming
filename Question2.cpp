9cea62636b9fef0bb5b7f2e22e1e4db2d3114d82
PouyaGohari <pouya.hmgohari@gmail.com>
2023-11-12 15:13:35 +0330

    Compelete question 2 by adding max_bridges

--- Question2.cpp
+++ Question2.cpp
@@ -27,7 +27,23 @@ fish_street getting_input(){
     return my_inputs;
 }
 
+int max_bridges(fish_street inputs){
+    vector<vector<int>> dp(inputs.number+1, vector<int>(inputs.number+1, 0));
+    for(int i = 1; i <= inputs.number; i++){
+        for(int j = 1; j <= inputs.number; j++){
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            for(int x = 1; x <= i; x++){
+                if(abs(inputs.streets[j-1] - inputs.fishes[x-1]) <= 4){
+                    dp[i][j] = max(dp[i][j], dp[x-1][j-1]+1);
+                }
+            }
+        }
+    }
+    return dp[inputs.number][inputs.number];
+}
+
 int main(){
     fish_street inputs = getting_input();
+    cout << max_bridges(inputs) << endl;
     return 0;
 }
